097569a6c4d6f735285f2205f2975780798761e8
Elaiza Paculanan <paculananelaizamae@gmail.com>
2024-04-05 14:55:42 -0400

    Deployed website

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return <div className="footer">Created by Elaiza Mae Paculanan © 2024</div>;
+};
+
+export default Footer;
